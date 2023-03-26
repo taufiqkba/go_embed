4974ec4c82efeb71bbbd2c1d3fae6c9f1a52405c
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-03-26 13:38:34 +0700

    Embed multiple files

--- embed_test.go
+++ embed_test.go
@@ -1,6 +1,7 @@
 package go_embed
 
 import (
+	"embed"
 	_ "embed"
 	"fmt"
 	"io/fs"
@@ -8,6 +9,8 @@ import (
 	"testing"
 )
 
+// Embed string
+//
 //go:embed version.txt
 var version string
 
@@ -15,6 +18,8 @@ func TestString(t *testing.T) {
 	fmt.Println(version)
 }
 
+// Embed file to []byte type (from image)
+//
 //go:embed logo.jpg
 var logo []byte
 
@@ -23,4 +28,26 @@ func TestByteArray(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+// Embed multiple files
+//
+//go:embed files/a.txt
+//go:embed files/b.txt
+//go:embed files/c.txt
+
+var files embed.FS
+
+func TestMultipleFile(t *testing.T) {
+	a, err := files.ReadFile("files/a.txt")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(a))
+
+	b, _ := files.ReadFile("files/b.txt")
+	fmt.Println(string(b))
+
+	c, _ := files.ReadFile("files/c.txt")
+	fmt.Println(string(c))
 }
